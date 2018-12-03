ddac31e28a99636c653c16380cfbc9124a0ed7ff
takatoh <takatoh.m@gmail.com>
2018-12-03 15:54:52 +0900

    main.go: Use html/template.

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"html/template"
 )
 
 func main() {
 	pattern := os.Args[1]
 	filenames, _ := filepath.Glob(pattern)
 
-	for _, filename := range filenames {
-		fmt.Println(filename)
+	t := template.Must(template.ParseFiles("index.tmpl"))
+	w, err := os.OpenFile("index.html", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	err = t.ExecuteTemplate(w, "index.tmpl", filenames)
+	if err != nil {
+		panic(err)
 	}
 }
